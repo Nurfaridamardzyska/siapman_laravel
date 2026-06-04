4c4a62d5d87f819d4a6e59ea0e6a7fd59e75c59b
Dyzka <160555433+Nurfaridamardzyska@users.noreply.github.com>
2026-06-05 04:01:51 +0700

    fix: Update URL face service ke production (188.166.184.53/face-api)

--- resources/views/superadmin/pengguna/create.blade.php
+++ resources/views/superadmin/pengguna/create.blade.php
@@ -217,7 +217,7 @@
 </div>
 
 <script>
-    const FACE_SERVICE_URL = "http://127.0.0.1:5001";
+    const FACE_SERVICE_URL = "http://188.166.184.53/face-api";
 
     document.addEventListener('DOMContentLoaded', function() {
         const form = document.querySelector('form');
